e7565f2cc1933903c54c4f9038f35745de05bea3
MarcoGomezGutierrez <marcogomezgutierrez@gmail.com>
2023-05-08 14:20:52 +0200

    Información detallada de la tabla temarios con toda la información agrupada: nombre_temario, nombre_nivel, enunciado_pregunta, respuesta1_texto, respuesta1_correcta, respuesta2_texto, respuesta2_correcta, respuesta3_texto, respuesta3_correcta, respuesta4_texto, respuesta4_correcta

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,6 +9,10 @@ class Home extends Component {
         try {
             const users = JSON.parse(localStorage.getItem("user"));
             const token = users.token;
+            /* Hago una petición al servidor para comprobar que el estado del token es valido y que su sesión todavía no haya expirado
+            En caso de una sesión abierta: redirigir a la pestaña loby
+            En caso de no existir una sesión o una sesión que haya caducado: se queda en la página actual.
+            */
             axios.post('http://localhost:8080/app/loby', {
                 token
             })
@@ -21,9 +25,11 @@ class Home extends Component {
                     }
                 })
                 .catch(error => {
+                    // TODO: Eliminar
                     console.error(error);
                 });
         } catch (err) {
+            // TODO: Eliminar
             console.error(err);
         }
 
--- server/src/module/game.js
+++ server/src/module/game.js
@@ -2,6 +2,39 @@ const express = require('express');
 const router = express.Router();
 const db  = require('../module/connection');
 
+router.get('/temarios', (req, res) => {
+    try {
+        db.query(`
+        SELECT temarios.nombre AS nombre_temario, niveles.nombre AS nombre_nivel, preguntas.enunciado AS enunciado_pregunta, 
+        respuestas1.texto AS respuesta1_texto, respuestas1.correcta AS respuesta1_correcta, 
+        respuestas2.texto AS respuesta2_texto, respuestas2.correcta AS respuesta2_correcta,
+        respuestas3.texto AS respuesta3_texto, respuestas3.correcta AS respuesta3_correcta,
+        respuestas4.texto AS respuesta4_texto, respuestas4.correcta AS respuesta4_correcta
+        FROM temarios 
+        INNER JOIN niveles ON temarios.id = niveles.id_temario 
+        INNER JOIN preguntas ON niveles.id = preguntas.id_nivel 
+        INNER JOIN respuestas AS respuestas1 ON preguntas.id = respuestas1.id_pregunta AND respuestas1.id = (4 * (preguntas.id - 1)) + 1
+        INNER JOIN respuestas AS respuestas2 ON preguntas.id = respuestas2.id_pregunta AND respuestas2.id = (4 * (preguntas.id - 1)) + 2
+        INNER JOIN respuestas AS respuestas3 ON preguntas.id = respuestas3.id_pregunta AND respuestas3.id = (4 * (preguntas.id - 1)) + 3
+        INNER JOIN respuestas AS respuestas4 ON preguntas.id = respuestas4.id_pregunta AND respuestas4.id = (4 * (preguntas.id - 1)) + 4
+        ORDER BY nombre_temario, nombre_nivel;
+        `, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send({
+                    msg: 'Error interno del servidor'
+                });
+            }
+            console.log(results);
+            return res.status(200).send({
+                results
+            });
+        });
+    } catch(err) {
+        console.log(err);
+    }
+});
+
 router.post('/insert', (req, res) => {
     const { temario, nivel, pregunta, respuesta } = req.body;
     console.log(respuesta);
@@ -17,7 +50,7 @@ router.post('/insert', (req, res) => {
             SELECT ?, @id_temario
             WHERE NOT EXISTS (SELECT 1 FROM niveles WHERE nombre = ? && id_temario = @id_temario);
             
-            SELECT id INTO @id_nivel FROM niveles WHERE nombre = ?;
+            SELECT id INTO @id_nivel FROM niveles WHERE nombre = ? && id_temario = @id_temario;
             
             INSERT INTO preguntas (enunciado, id_nivel) VALUES(?, @id_nivel);
             
